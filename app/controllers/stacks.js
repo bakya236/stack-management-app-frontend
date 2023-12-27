82e6024ae7eb689dd42aa78ead5a75580d85be32
Boobalan Ravichandran <116268868+BoobalanRP@users.noreply.github.com>
2023-12-27 17:17:15 +0530

    implemented email verification page

--- app/controllers/stacks.js
+++ app/controllers/stacks.js
@@ -1,5 +1,26 @@
 import Ember from 'ember';
+//import {get, set } from '@ember/object';
 
 export default Ember.Controller.extend({
-    
+  isEmailPopupOpen: false,
+  email: '',
+
+  actions: {
+    openEmailPopup() {
+      this.set('isEmailPopupOpen', true);
+    },
+
+    closeEmailPopup() {
+      this.set('isEmailPopupOpen', false);
+    },
+
+    verifyEmail() {
+      if (this.email.trim() !== '') {
+      localStorage.setItem('email', this.email);
+      this.set('isEmailPopupOpen', false); 
+      this.set('email', ''); 
+    }else {
+      alert('Please enter a valid email');
+    }
+    }}
 });
